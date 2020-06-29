55992413ab460f5b600fa00efd51ee5ccf841af6
unknown <1546525458@qq.com>
2020-06-29 13:48:16 +0800

    下载vant banner-swipe组件调用

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
 Vue.config.productionTip = false
+
+Vue.use(Vant)
 
 /* eslint-disable no-new */
 new Vue({
